4a0568eb7a1bed2832ebc5341ea78b5cfe5674ba
Maden51 <madenon123@gmail.com>
2022-11-08 10:50:46 +0300

    console log removed

--- js/newProject.js
+++ js/newProject.js
@@ -7,7 +7,6 @@ const customSelect = document.getElementsByClassName('custom-select');
 
 for (i = 0; i < customSelect.length; i++) {
   const selElement = customSelect[i].getElementsByTagName('select')[0];
-  console.log(selElement)
   const selectedItem = document.createElement('div');
   selectedItem.setAttribute('class', 'select-selected');
   selectedItem.innerHTML = selElement.options[selElement.selectedIndex].innerHTML;
@@ -40,7 +39,6 @@ for (i = 0; i < customSelect.length; i++) {
   selectedItem.addEventListener('click', function(e) {
     e.stopPropagation();
     closeAllSelect(this);
-    console.log(this);
     if (this.classList.contains('select-arrow-active')) {
       this.nextSibling.classList.add('select-hide');
     } else {
