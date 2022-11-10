fcf334e2655aefb7485d5e5d5d51c5df8e71e14f
Maden51 <madenon123@gmail.com>
2022-11-10 18:14:19 +0300

    initial commit

--- js/custom-cursor.js
+++ js/custom-cursor.js
@@ -8,7 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
       const target = e.target // определяем, где находится курсор
       if (!target) return
 
-      if (target.closest('a')) { // если курсор наведён на ссылку
+      if (target.closest('a') ||
+          target.closest('.tab-wrapper') ||
+          target.closest('.swiper-button-next') ||
+          target.closest('.swiper-button-prev') ||
+          target.closest('button') ||
+          target.closest('.checkbox')) { // если курсор наведён на ссылку
         el.classList.add('custom-cursor_active') // элементу добавляем активный класс
       } else { // иначе
         el.classList.remove('custom-cursor_active') // удаляем активный класс
@@ -19,35 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
           target.closest('.dropdown_content a') ||
           target.closest('.wrap_button button') ||
           target.closest('.wrap_button_white button') ||
-          target.closest('.white_a')
-          ) {
-        el.classList.add('custom-cursor_dark')
-      } else {
-        el.classList.remove('custom-cursor_dark')
-      }
-
-      let translate = (a, b) => `translate(${a}, ${b})`;
-      let px = 'px';
-
-      el.style.transform = translate(e.pageX + px, e.pageY + px) // задаём элементу позиционирование слева
-    });
-
-    window.addEventListener('scroll', e => { // при движении курсора
-      const target = e.target // определяем, где находится курсор
-      if (!target) return
-
-      if (target.closest('a')) { // если курсор наведён на ссылку
-        el.classList.add('custom-cursor_active') // элементу добавляем активный класс
-      } else { // иначе
-        el.classList.remove('custom-cursor_active') // удаляем активный класс
-      }
-
-      if (target.closest('.content-logo') || 
-          target.closest('.swiper-slide.swiper-slide-active') ||
-          target.closest('.dropdown_content a') ||
-          target.closest('.wrap_button button') ||
-          target.closest('.wrap_button_white button') ||
-          target.closest('.white_a')
+          target.closest('.white_a') ||
+          target.closest('.cases-btn') ||
+          target.closest('.about-btn')
           ) {
         el.classList.add('custom-cursor_dark')
       } else {
